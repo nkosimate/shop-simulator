b3208392f7bf561cad330133ca8fb7b5db44cff5
nkosimate <nkosimate@gmail.com>
2022-06-27 00:38:37 +0100

    db

--- final/server.js
+++ final/server.js
@@ -1,8 +1,24 @@
-var express = require('express');
-var app = express();
-
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/shop";
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
 
 app.use(express.static('public'));
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
+
+var db;
+
+//Sets up the Mongodb database//
+MongoClient.connect(url, function (err, database) {
+    if (err) throw err;
+    db = database;
+    app.listen(8080);
+    console.log('listening on 8080');
+});
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -12,15 +28,19 @@ app.set('view engine', 'ejs');
 //Default goes to Home//
 app.get('/', function (req, res) {
     res.render('pages/home');
-  });
+});
+//Gets Home.ejs//
+app.get('/home', function (req, res) {
+    res.render('pages/home');
+});
 
 //login page
-app.get('/login',function(req,res){
+app.get('/login', function (req, res) {
     res.render('pages/login')
 });
 
 //create account page
-app.get('createAccount',function(req,res){
+app.get('createAccount', function (req, res) {
     res.render('pages/createAcc')
 });
 
