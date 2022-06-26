dc2d23c93c60bd1d666136b3165ed7bfd318424c
nkosimate <nkosimate@gmail.com>
2022-06-26 23:56:32 +0100

    ejs

--- final/server.js
+++ final/server.js
@@ -1,10 +1,37 @@
+const MongoClient = require('mongodb').MongoClient;
+const url = "mongodb://localhost:27017/shop";
 var express = require('express');
 var app = express();
+var db;
 
 app.use(express.static('public'))
+app.use(express.urlencoded({extended:true}))
 
+// set the view engine to ejs
+app.set('view engine', 'ejs');
+
+
+//Sets up the Mongodb database//
+MongoClient.connect(url, function (err, database) {
+    if (err) throw err;
+    db = database;
+    app.listen(8080);
+    console.log('listening on 8080');
+  });
+
+//home page
 app.get('/', function (req, res) {
-    res.send("Hello world! by express");
+    res.render('/home');
+});
+
+//login page
+app.get('/login',function(req,res){
+    res.render('/login')
+});
+
+//create account page
+app.get('createAccount',function(req,res){
+    res.render('/createAcc')
 });
 
 app.listen(8080);
