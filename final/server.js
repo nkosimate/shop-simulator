501c8e28f34c4225b8539b450f384c1c504d1bcd
nkosimate <nkosimate@gmail.com>
2022-06-30 12:34:20 +0100

    oject.keys

--- final/server.js
+++ final/server.js
@@ -150,8 +150,9 @@ app.post('/dologin', function (req, res) {
 app.post('/buyproduct1', function (req, res) {
    db.collection('product').findOne({ "name": "Yeezt 720"}, { "price": 1},function (err, result) {
         if (err) throw err;
+        var results = Object.keys(result);
         console.log("the old price");
-        console.log(typeof result);
+        console.log(results);
     });
    
 
