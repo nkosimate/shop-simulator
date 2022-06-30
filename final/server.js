8d1e4449d7a8d78ccc476c40ca2c178dbb1371ff
nkosimate <nkosimate@gmail.com>
2022-06-30 21:02:57 +0100

    stock and others

--- final/server.js
+++ final/server.js
@@ -145,17 +145,21 @@ app.post('/dologin', function (req, res) {
 
 
 app.post('/buyproduct1', function (req, res) {
-    db.collection('product').findOne({ "name": "Yeezy 350" }, { "price": 1 ,"stock.p1":1,"balance":1,"total":1}, function (err, result) {
+    db.collection('product').findOne({ "name": "Yeezy 350" }, { "price": 1 }, function (err, result) {
         if (err) throw err;
         var results = Object.values(result);
-        console.log("the result");
-        console.log(result);
-        //var oldprice = results[1];
-        //var newPrice = oldprice + 5;
-        //console.log(newPrice)
+        var oldprice = results[1];
+        var newPrice = oldprice + 5;
+        var currentuser = req.session.currentuser;
         //update price in product db
         //update stock p1, balance and total in user db for that user
         //update totals.user for admin in user db
+        db.collection('users').findOne({ "name": currentuser }, { "stock.p1": 1, "balance": 1, "total": 1 }, function (err, userresults) {
+            if (err) throw err;
+            var results = Object.values(result);
+            console.log("the result");
+            console.log(results);
+        })
         /* var newvalueProduct = { $set: { price: newPrice } };
         var newvalueStock =  { $set:{ stock.p1: }};
         db.collection('product').updateOne({ "name": "Yeezy 350" }, newvalueProduct, function (err, result) {
