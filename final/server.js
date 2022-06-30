7ffed9e6e7e128cd4e5272f6701ddd337143f923
nkosimate <nkosimate@gmail.com>
2022-06-30 10:36:21 +0100

    fix

--- final/server.js
+++ final/server.js
@@ -4,7 +4,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const session = require('express-session');
 const app = express();
-let calculation = require('/public/script/js')
+
 
 
 
