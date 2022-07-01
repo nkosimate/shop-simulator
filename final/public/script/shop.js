efa09ac78d85da205e71adc0285f982c3a236b59
nkosimate <nkosimate@gmail.com>
2022-07-01 14:13:16 +0100

    buyp1

--- final/public/script/shop.js
+++ final/public/script/shop.js
@@ -1,9 +1,7 @@
 var socket =io();
-$('#myform1').submit(function(){
+$('#buyp1').onclick(function(){
     var oldPriceString = $('pprice');
     var intPrice = parseInt(oldPriceString);
     console.log("the price is " + intPrice + " of data type " + typeof(intPrice));
-    if (intPrice){
-    }
 
 })
