1610f768eb8084e1d67795ff1857ae80426ee127
nkosimate <nkosimate@gmail.com>
2022-07-01 14:09:36 +0100

    log

--- final/public/script/shop.js
+++ final/public/script/shop.js
@@ -2,8 +2,8 @@ var socket =io();
 $('#myform1').submit(function(){
     var oldPriceString = $('pprice');
     var intPrice = parseInt(oldPriceString);
+    console.log("the price is " + intPrice + " of data type " + typeof(intPrice));
     if (intPrice){
-        console.log("the price is " + intPrice + " of data type " + typeof(intPrice));
     }
 
 })
