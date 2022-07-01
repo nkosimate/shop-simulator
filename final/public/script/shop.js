ac3095028ded515189904f645b7140f62998689b
nkosimate <nkosimate@gmail.com>
2022-07-01 14:32:10 +0100

    .val

--- final/public/script/shop.js
+++ final/public/script/shop.js
@@ -1,6 +1,6 @@
 var socket =io();
 $('#buyp1').click(function(){
-    var oldPriceString = $('pprice').text();
+    var oldPriceString = $('pprice').val();
     alert("the price is " + oldPriceString + " of data type " + typeof(oldPriceString));
 
 })
