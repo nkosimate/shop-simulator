0be84d3451a64dbc6fa9f4ad0116313b822971cd
nkosimate <nkosimate@gmail.com>
2022-07-01 13:57:41 +0100

    shop.js

--- final/public/script/shop.js
+++ final/public/script/shop.js
@@ -1,7 +1,1 @@
-// Get the button to open Own Quote pop up//
-var btn = document.getElementById("buybtn1");
-
-// When the user clicks the button, open the pop up//
-btn.onclick = function () {
-    alert("button was clicked");
-}
+var socket =io();
