afb07cbae99fe63021062721ef76e202a7b81836
nkosimate <nkosimate@gmail.com>
2022-07-01 16:02:37 +0100

    string it

--- final/public/script/shop.js
+++ final/public/script/shop.js
@@ -17,7 +17,7 @@ socket.on('purchase product1', function (price) {
     const textnode = document.createTextNode(toString(price));
     element.appendChild(textnode);
 
-    const oldlabel = document.getElementById('pprice').textContent;
+    const oldlabel = toString(document.getElementById('pprice').textContent);
     document.body.replaceChild(element,oldlabel);
 
 
